b24ad5d57b2ac2940de271eb714aa6ec28ade777
SXR-Star <2087545695@qq.com>
2026-04-28 19:22:41 +0800

    移除冗余的密码验证器配置：系统无用户注册功能，AUTH_PASSWORD_VALIDATORS形同虚设

--- 新生报到系统/settings.py
+++ 新生报到系统/settings.py
@@ -64,21 +64,6 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
